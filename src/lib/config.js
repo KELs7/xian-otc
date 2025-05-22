b69b080c8e7e3162823ccd72869db77ff96dfa37
kels7 <hiketabo@gmail.com>
2025-05-22 12:29:24 +0000

    refactor: change configs based on current type of node

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,9 +1,70 @@
-export const config = {
-    otcContract: "con_otc", // CHANGE
-    currencySymbol: "XIAN", // CHANGE
-    blockExplorer: "https://testnet-explorer.xian.org", // NOT USED
-    masternode: "https://testnet.xian.org", // NOT USED
-    graphqlEndpoint: "https://node.xian.org/graphql", // CHANGE
-    webSocketUrl: "wss://testnet.xian.org/websocket", // NOT USED
-    otcFeePercentage: 0.005 
+import { get } from "svelte/store";
+import { networkType } from "./store";
+
+const config = {
+    otcContract: {
+        "testnet": "con_otc",
+        "mainnet": "con_otc"
+    },
+    otcFeePercentage: {
+        "testnet": 0.005,
+        "mainnet": 0.005
+    }, 
+    masternode: {
+        "testnet": "https://testnet.xian.org",
+        "mainnet": "https://node.xian.org"
+    },
+    graphqlEndpoint: {
+        "testnet": "https://testnet.xian.org/graphql",
+        "mainnet": "https://node.xian.org/graphql"
+    },
+    webSocketUrl: {
+        "testnet": "https://testnet.xian.org/websocket",
+        "mainnet": "https://node.xian.org/websocket"
+    }   
+}
+
+export const getOtcContract = ()=>{
+    const network = get(networkType);
+    if (network === "testnet"){
+        return config.otcContract.testnet
+    }else{
+        return config.otcContract.mainnet
+    }
+}
+
+export const getOtcFeePercentage = ()=>{
+    const network = get(networkType);
+    if (network === "testnet"){
+        return config.otcFeePercentage.testnet
+    }else{
+        return config.otcFeePercentage.mainnet
+    }
+}
+
+export const getMasterNode = ()=>{
+    const network = get(networkType);
+    if (network === "testnet"){
+        return config.masternode.testnet
+    }else{
+        return config.masternode.mainnet
+    }
+}
+
+export const getGraphqlEndpoint = ()=>{
+    const network = get(networkType);
+    if (network === "testnet"){
+        return config.graphqlEndpoint.testnet
+    }else{
+        return config.graphqlEndpoint.mainnet
+    }
+}
+
+export const getWebSocketUrl = ()=>{
+    const network = get(networkType);
+    if (network === "testnet"){
+        return config.webSocketUrl.testnet
+    }else{
+        return config.webSocketUrl.mainnet
+    }
 }
--- src/lib/store.js
+++ src/lib/store.js
@@ -1,4 +1,5 @@
 import { writable } from "svelte/store";
 
+export const networkType = writable("testnet");
 export const walletAddressElementValue = writable("Connecting...");
 export const transactionInfo = writable({});
