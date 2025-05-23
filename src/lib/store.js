beb5d251996a860296f7e86a7026082bd24ec55e
kels7 <hiketabo@gmail.com>
2025-05-23 13:06:30 +0000

    feat: switch to mainnet

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,5 +1,5 @@
 import { writable } from "svelte/store";
 
-export const networkType = writable("testnet");
+export const networkType = writable("mainnet");
 export const walletAddressElementValue = writable("Connecting...");
 export const transactionInfo = writable({});
